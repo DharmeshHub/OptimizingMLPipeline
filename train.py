206317986e9c8961ab44d65b7fe8307acd48c916
DharmeshHub <73326514+DharmeshHub@users.noreply.github.com>
2020-12-20 13:10:24 -0600

    Update train.py

--- train.py
+++ train.py
@@ -14,8 +14,9 @@ from azureml.data.dataset_factory import TabularDatasetFactory
 # Data is located at:
 # "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
 
-ds = Dataset.Tabular.from_delimited_files(path=datastore_path)
-print(ds.to_pandas_dataframe())
+vURLFile="https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+ds = TabularDatasetFactory.from_delimited_files(path=vURLFile)
+pddf = ds.to_pandas_dataframe()
     
 x, y = clean_data(ds)
 
